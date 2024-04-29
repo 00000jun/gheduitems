001e361e210cd40e792fcb1f0b809f6d28aad593
00000jun <166696851+00000jun@users.noreply.github.com>
2024-04-29 15:25:57 +0900

    메인코드

--- main.py
+++ main.py
@@ -6,14 +6,46 @@ st.header("물품조사 페이지", divider='rainbow')
 # 데이터프레임 처리
 df = pd.read_excel('items_test.xlsx')
 
-with st.expander('expn_1', expanded=False):
-    st.dataframe(df, width=3000, height=500)
 
-# 에디트 테이블
-with st.expander('expn_2', expanded=True):
-    result = st.data_editor(df, num_rows='dynamic')
+# 이미지 url 처리
+url_df = pd.read_table('result_imges_url.txt', header=None) # 이미지 url txt 파일 불러오기
+url_df = url_df[0]
+
+code_list = [] # 물품목록번호 리스트
+img_url_list = [] # 이미지주소 리스트
+
+for line in url_df:
+    code, url = line.split(' : ') # 물품목록번호와 이미지주소 분리
+    code_list.append(code)
+    img_url_list.append(url)
+
+pre_url_df = pd.DataFrame({'물품목록번호': code_list, '이미지주소': img_url_list}) # (물품목록번호, 이미지주소) 데이터프레임 생성
+
+
+# 데이터프레임 병합 : 물품목록번호를 기준으로 이미지주소를 병합
+merged_df = pd.merge(df, pre_url_df, on='물품목록번호', how='left')
+
+
+# 데이터프레임 후처리 (컬럼 인덱싱)
+merged_df = merged_df[['순','이미지주소_x', '물품목록번호', '물품분류명', '품명규격', '등록수량', '검수수량']]
+
+
+# 처리 후 데이터프레임
+with st.expander('검수용 데이터프레임', expanded=True):
+    result = st.data_editor(
+        merged_df,
+        column_config={
+            "이미지주소_x": st.column_config.ImageColumn(
+                "미리보기", help="클릭시 확대"
+            )
+        },
+        num_rows='dynamic',
+        width=7000,
+        height=3000,
+        )
+
 
 # 저장 버튼
-if st.button('저장'):
+if st.button('저장', help='검수수량 입력 후 저장 버튼을 누르면 데이터 저장'):
     result.to_excel('items_test.xlsx', index=False)
     st.success('저장 완료')
